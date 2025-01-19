8546a382a4f1c9c311da48d6fe0f21c12f3016b4
Vaishnavi Thorat <vaishnavit582@gmail.com>
2025-01-19 09:10:08 +0530

    new changes

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,6 @@ const loanFundingRoute=require("./routes/loanFundingRoute");
 const walletRoute=require("./routes/walletRoute");
 
 
-const walletRoute=require("./routes/walletRoute")
 
 const portfolioRoute=require('./routes/portFolio')
 
